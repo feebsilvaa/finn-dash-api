3f0d6db6cf5bda0c16c0431690a04db144c1fd4e
Fernando Barbosa <fernando.barbosa@meta.com.br>
2019-11-07 18:55:02 -0300

    iniciando configurações do projeto

---
+++ src/main/java/br/com/feedev/finndash/api/FinnDashApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.feedev.finndash.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinnDashApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinnDashApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/feedev/finndash/api/FinnDashApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.feedev.finndash.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinnDashApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
